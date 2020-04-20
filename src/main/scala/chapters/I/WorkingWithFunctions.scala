83302948b1702d3d0658aa74ea6f6b588c26bafd
David Huebel <davidhuebel@gmail.com>
2020-04-20 11:44:46 -0500

    Working with functions and working with pattern matching pass tests

--- src/main/scala/chapters/I/WorkingWithFunctions.scala
+++ src/main/scala/chapters/I/WorkingWithFunctions.scala
@@ -7,12 +7,12 @@ object WorkingWithFunctions {
   /**
     * Implement a function that adds two Ints together.
     */
-  lazy val addTwoNumbers: (Int, Int) => Int = ???
+  lazy val addTwoNumbers: (Int, Int) => Int = (x: Int, y: Int) => x + y
 
   /**
     * Use `addTwoNumbers` defined above sum the values contained in `listOfInts`
     *
     */
-  lazy val sumOflistOfInts: Int = ???
+  lazy val sumOflistOfInts: Int = listOfInts.reduce(addTwoNumbers)
 
 }
--- src/main/scala/chapters/I/WorkingWithPatternMatching.scala
+++ src/main/scala/chapters/I/WorkingWithPatternMatching.scala
@@ -9,6 +9,15 @@ object WorkingWithPatternMatching {
     * - For numbers > 5, return the "i is greater than 5." where `i` is the string representation of the Int.
     *
     */
-  lazy val numberNamesUpToFive: Int => String = ???
+  lazy val numberNamesUpToFive: Int => String = (num: Int) => {
+    num match {
+      case 1 => "one"
+      case 2 => "two"
+      case 3 => "three"
+      case 4 => "four"
+      case 5 => "five"
+      case n => s"$n is greater than 5."
+    }
+  }
 
 }
